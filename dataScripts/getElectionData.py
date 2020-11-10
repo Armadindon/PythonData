8ab931ffe613787476ca05055be41fa32e043b38
PERRIN Baptiste <baptisteperrin77@gmail.com>
2020-11-10 09:53:59 +0100

    Fix script récupération DATA

--- dataScripts/getElectionData.py
+++ dataScripts/getElectionData.py
@@ -11,6 +11,21 @@ if __name__ == "__main__":
     fileCommunes = open("data/communes.csv")
     communesReader = DictReader(fileCommunes)
 
+    fields = [
+        "code_insee","nom",
+        "votes_MLEPEN","pourcentage_inscrit_MLEPEN","pourcentage_exprime_MLEPEN",
+        "votes_EMACRON","pourcentage_inscrit_EMACRON","pourcentage_exprime_EMACRON",
+        "votes_JLMELENCHON","pourcentage_inscrit_JLMELENCHON","pourcentage_exprime_JLMELENCHON",
+        "votes_FFILLON","pourcentage_inscrit_FFILLON","pourcentage_exprime_FFILLON",
+        "votes_NDUPONTAIGNAN","pourcentage_inscrit_NDUPONTAIGNAN","pourcentage_exprime_NDUPONTAIGNAN",
+        "votes_BHAMON","pourcentage_inscrit_BHAMON","pourcentage_exprime_BHAMON",
+        "votes_PPOUTOU","pourcentage_inscrit_PPOUTOU","pourcentage_exprime_PPOUTOU",
+        "votes_NARTHAUD","pourcentage_inscrit_NARTHAUD","pourcentage_exprime_NARTHAUD",
+        "votes_JLASSALLE","pourcentage_inscrit_JLASSALLE","pourcentage_exprime_JLASSALLE",
+        "votes_JCHEMINADE","pourcentage_inscrit_JCHEMINADE","pourcentage_exprime_JCHEMINADE",
+        "votes_FASSELINEAU","pourcentage_inscrit_FASSELINEAU","pourcentage_exprime_FASSELINEAU",
+        ]
+
     partiByCandidate = {
         "Mme Marine LE PEN" : ("Front National", "MLEPEN"),
         "M. Emmanuel MACRON" : ("En Marche", "EMACRON"),
@@ -40,7 +55,7 @@ if __name__ == "__main__":
         }
 
         treeDept = html.fromstring(deptDataRequest.content.decode("utf-8"))
-        resultatsVote = treeDept.xpath('//table[@class="table table-bordered tableau-resultats-listes"][2]/tbody/tr')[1:] #On veut les résultats du second tour
+        resultatsVote = treeDept.xpath('//table[@class="table table-bordered tableau-resultats-listes"][2]/tbody/tr') #On veut les résultats du second tour
         #On construit la map avec les résultats
         for resultByCandidate in resultatsVote:
             dataByCandidate = list(resultByCandidate.xpath("./td/text()"))
@@ -48,24 +63,16 @@ if __name__ == "__main__":
             resultItem["pourcentage_inscrit_" +partiByCandidate[dataByCandidate[0]][1]] = float(dataByCandidate[2].replace(",","."))
             resultItem["pourcentage_exprime_" +partiByCandidate[dataByCandidate[0]][1]] = float(dataByCandidate[3].replace(",","."))
         
+        if not all(x in resultItem.keys() for x in fields):
+            print("Problèmes de données !", resultItem,)
+            print("Il manque", list(filter(lambda x: x not in resultItem.keys(), fields)),"\n")
+            continue
+        
         results.append(resultItem)
 
     print("On enregistre les données")
     with open("data/votes_communes.csv","w+",newline="") as csvData:
-        fields = [
-        "code_insee","nom",
-        "votes_MLEPEN","pourcentage_inscrit_MLEPEN","pourcentage_exprime_MLEPEN",
-        "votes_EMACRON","pourcentage_inscrit_EMACRON","pourcentage_exprime_EMACRON",
-        "votes_JLMELENCHON","pourcentage_inscrit_JLMELENCHON","pourcentage_exprime_JLMELENCHON",
-        "votes_FFILLON","pourcentage_inscrit_FFILLON","pourcentage_exprime_FFILLON",
-        "votes_NDUPONTAIGNAN","pourcentage_inscrit_NDUPONTAIGNAN","pourcentage_exprime_NDUPONTAIGNAN",
-        "votes_BHAMON","pourcentage_inscrit_BHAMON","pourcentage_exprime_BHAMON",
-        "votes_PPOUTOU","pourcentage_inscrit_PPOUTOU","pourcentage_exprime_PPOUTOU",
-        "votes_NARTHAUD","pourcentage_inscrit_NARTHAUD","pourcentage_exprime_NARTHAUD",
-        "votes_JLASSALLE","pourcentage_inscrit_JLASSALLE","pourcentage_exprime_JLASSALLE",
-        "votes_JCHEMINADE","pourcentage_inscrit_JCHEMINADE","pourcentage_exprime_JCHEMINADE",
-        "votes_FASSELINEAU","pourcentage_inscrit_FASSELINEAU","pourcentage_exprime_FASSELINEAU",
-        ]
+
         writer = DictWriter(csvData, fieldnames=fields)
         
         writer.writeheader()
