56e5dfc67f5e6eb505d4a1ac575fb867c1b407ac
PERRIN Baptiste <baptisteperrin77@gmail.com>
2021-01-08 09:38:15 +0100

    Add a map callback on dash

--- main.py
+++ main.py
@@ -1,33 +1,61 @@
 import dash
+import json
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
 import numpy as np
+from dash.dependencies import Input, Output
 
 #panels import
-import panels.electionMap
+from panels.electionMap import ChloroMap
 
 def toLength(code, expectedLength):
     return "0"*(expectedLength-len(code)) + str(code)
 
 app = dash.Dash(__name__)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
 dataset = pd.read_csv("data/votes_departements.csv")
+locations = []
+with open("data/departements.geojson","r") as f:
+    locations = json.load(f)
 
+
+#On initialise les panels
+chloropethMap = ChloroMap(dataset,locations)
+
+#On gère les callbacks
+deptSelected = "-1"
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
+
+    html.P(id='placeholder'),
 
     html.Div(children='''
         Dash: A web application framework for Python.
     '''),
     
-    dcc.Graph(figure=panels.electionMap.panel(dataset.copy()))
+    dcc.Graph(
+        id="electionMap",
+        figure=chloropethMap.mapPanel
+        )
 
 ])
+
+@app.callback(
+    Output("placeholder", "children"),
+    [Input("electionMap","clickData")]
+)
+def update_map(data):
+    global deptSelected
+    print(deptSelected, data["points"][0]["customdata"][0])
+    
+    if deptSelected == data["points"][0]["customdata"][0]:
+        print("On update la map !")
+
+    deptSelected = data["points"][0]["customdata"][0]
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
