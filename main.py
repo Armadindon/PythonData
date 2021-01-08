52912ae5369598bbe6d8de2f88dbbcb31f5ef874
PERRIN Baptiste <baptisteperrin77@gmail.com>
2021-01-08 12:55:07 +0100

    Work on callback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import dash
 import json
+import os
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
@@ -13,16 +14,25 @@ from panels.electionMap import ChloroMap
 def toLength(code, expectedLength):
     return "0"*(expectedLength-len(code)) + str(code)
 
-app = dash.Dash(__name__)
+def loadDept(code):
+    with open(os.path.join("data", "geojsonByDpt", code ,"communes.geojson")) as f:
+        result = json.load(f)
+    return result
 
+app = dash.Dash(__name__, prevent_initial_callbacks=True)
+
+#On Load les datasets
 dataset = pd.read_csv("data/votes_departements.csv")
-locations = []
+datasetCity = pd.read_csv("data/votes_communes.csv")
+baseLocations = []
 with open("data/departements.geojson","r") as f:
-    locations = json.load(f)
+    baseLocations = json.load(f)
 
+locations = baseLocations.copy()
+cityLocations = []
 
 #On initialise les panels
-chloropethMap = ChloroMap(dataset,locations)
+chloropethMap = ChloroMap(dataset,datasetCity,locations,cityLocations)
 
 #On gère les callbacks
 deptSelected = "-1"
@@ -38,23 +48,26 @@ app.layout = html.Div(children=[
     
     dcc.Graph(
         id="electionMap",
-        figure=chloropethMap.mapPanel
-        )
-
+        figure=chloropethMap.mapPanel,
+    )
 ])
 
 @app.callback(
-    Output("placeholder", "children"),
+    Output("electionMap", "figure"),
     [Input("electionMap","clickData")]
 )
 def update_map(data):
     global deptSelected
-    print(deptSelected, data["points"][0]["customdata"][0])
-    
-    if deptSelected == data["points"][0]["customdata"][0]:
-        print("On update la map !")
-
+    cityLocations = []
     deptSelected = data["points"][0]["customdata"][0]
+    locations = baseLocations.copy()
+    print("On update la map !")
+    cityLocations = loadDept(deptSelected)
+    locations["features"] = list(filter(lambda x: not x["properties"]["code"] == deptSelected, locations["features"]))
+    chloropethMap.update(dataset,datasetCity,locations,cityLocations)
+    chloropethMap.generateMap()
+
+    return chloropethMap.mapPanel
 
 
 if __name__ == '__main__':
